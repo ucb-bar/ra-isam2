4450baa6f1a6ffb60c8c786bc44ee1963181754c
rogerhh <rogerhh@umich.edu>
2024-03-24 04:49:49 +0000

    uncomment w10000 test generation

--- timing/python/generate_dataset_vio.sh
+++ timing/python/generate_dataset_vio.sh
@@ -16,11 +16,11 @@ dirname=vio_w10000_steps-${num_steps}_vio_lag-${vio_lag}
 
 mkdir -p ${BUILD_DIR}/${dirname}
 
-# $BUILD_DIR/timing/testGtsamIncremental-datasetgen -f w10000 --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${dirname}
+$BUILD_DIR/timing/testGtsamIncremental-datasetgen -f w10000 --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${dirname}
 
 mkdir -p ${HEADER_DIR}/${dirname}
 
-# python3 $SCRIPT_DIR/generate_dataset_vio.py --indir $BUILD_DIR/${dirname} --outdir $HEADER_DIR/${dirname} --start_step 2 --end_step $((${num_steps} - 1)) --vio --vio_lag ${vio_lag} --vio_scale ${vio_scale}
+python3 $SCRIPT_DIR/generate_dataset_vio.py --indir $BUILD_DIR/${dirname} --outdir $HEADER_DIR/${dirname} --start_step 2 --end_step $((${num_steps} - 1)) --vio --vio_lag ${vio_lag} --vio_scale ${vio_scale}
 
 # ========================== CHANGE HERE ========================== #
 num_steps=300
