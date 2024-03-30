ce22829822efcf4285f9afdbce9ed2c5598deb54
rogerhh <rogerhh@umich.edu>
2024-03-30 07:38:08 +0000

    generator script

--- timing/python/generate_dataset_incremental.sh
+++ timing/python/generate_dataset_incremental.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 HEADER_DIR=$SCRIPT_DIR/../baremetal_tests
 BUILD_DIR=$SCRIPT_DIR/../../build
 
-$SCRIPT_DIR/build.sh
+# $SCRIPT_DIR/build.sh
 
 # ========================== CHANGE HERE ========================== #
 start_step=2
@@ -17,7 +17,7 @@ dirname=incremental_w10000_steps-${num_steps}
 
 mkdir -p ${BUILD_DIR}/${dirname}
 
-$BUILD_DIR/timing/testGtsamIncremental-datasetgen -f w10000 --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${dirname}
+# $BUILD_DIR/timing/testGtsamIncremental-datasetgen -f w10000 --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${dirname}
 
 mkdir -p ${HEADER_DIR}/${dirname}
 
@@ -35,11 +35,11 @@ mkdir -p ${BUILD_DIR}/${dirname}
 mkdir -p ${BUILD_DIR}/${seed_dirname}
 
 # First generate seed
-$BUILD_DIR/timing/testGtsamIncremental3D-datasetgen -f sphere2500 --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${seed_dirname}
+# $BUILD_DIR/timing/testGtsamIncremental3D-datasetgen -f sphere2500 --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${seed_dirname}
 
-$SCRIPT_DIR/generate_relin_keys_file.sh ${seed_dirname} $start_step $num_steps
+# $SCRIPT_DIR/generate_relin_keys_file.sh ${seed_dirname} $start_step $num_steps
 
-$BUILD_DIR/timing/testGtsamIncremental3D-datasetgen -f sphere2500 --relin_thresh 100000 --relin_keys_file ${HEADER_DIR}/${seed_dirname}_relin_keys --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${dirname}
+# $BUILD_DIR/timing/testGtsamIncremental3D-datasetgen -f sphere2500 --relin_thresh 100000 --relin_keys_file ${HEADER_DIR}/${seed_dirname}_relin_keys --num_steps $num_steps --dataset_outdir ${BUILD_DIR}/${dirname}
 
 mkdir -p ${HEADER_DIR}/${dirname}
 
